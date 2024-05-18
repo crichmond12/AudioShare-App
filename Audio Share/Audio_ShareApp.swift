6a070e24f4ea80240f87bcbc6de3a16fcbda500c
Christian <chriarich12@gmail.com>
2024-05-18 17:19:06 -0500

    Initial Commit

---
+++ Audio Share/Audio_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_ShareApp.swift
+//  Audio Share
+//
+//  Created by Christian Richmond on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Audio_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
